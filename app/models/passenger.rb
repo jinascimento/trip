3fa9529b7ffc4ee1906b74636a804a3a2822cb0a
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-08-27 15:28:13 -0300

    Change armchair

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,15 @@
 class Passenger < ApplicationRecord
+  before_save :occupy_armchair
+  before_destroy :vacate_armchair
   belongs_to :bus, optional: true
+
+  def occupy_armchair
+    bus = Bus.find(bus_id)
+    bus.update(armchair: bus.armchair -= 1)
+  end
+
+  def vacate_armchair
+    bus = Bus.find(bus_id)
+    bus.update(armchair: bus.armchair += 1)
+  end
 end
